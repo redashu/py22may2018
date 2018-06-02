85167374634934cdf54b135378b811ff6137d09b
root <root@station216.example.com>
2018-06-02 13:08:09 +0530

    function in python

---
+++ fun.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+import  time
+#  defining function without arg
+def  motd():
+	print "hello ALL,  time is ",time.ctime()
+	print  8000
+#  fun with arg  
+
+def  add(x,y):
+	print  x+y
+
+def   new():
+	print  "\n"*50
+
+
+
